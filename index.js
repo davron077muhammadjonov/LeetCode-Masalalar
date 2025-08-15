e588b4e6c62f016899bc965ef31f755d566a83fc
davron077muhammadjonov <muhammadjonovdavron077@gmail.com>
2025-08-15 15:44:36 +0500

    leetcode

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// var isPowerOfFour = function(n) {0
+//     if(n==4/n**4){
+//         console.log(true);
+//      }else return false
+//     }
+
+// console.log(isPowerOfFour(16));0
+
+
+
+var getSum = function(a, b) {
+    
+};
